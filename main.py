dc11606a6cc6f3120fb371dab028a8b3fb86097f
Rhythm Mehta <40776621+rhythmehta@users.noreply.github.com>
2022-04-15 14:27:38 -0700

    Update and rename main.py to serve.py

--- main.py
+++
@@ -1,6 +0,0 @@
-from app import models, data, query
-
-if __name__ == '__main__':
-    models
-    data
-    query
---
+++ serve.py
@@ -0,0 +1,6 @@
+from app import create, insert_data, query_data
+
+if __name__ == '__main__':
+    create
+    insert_data
+    query_data
